325c8a9a942170af856489afe297be3b5cea7a84
JeremyKalmus <Jeremykalmus@gmail.com>
2021-01-23 15:00:27 -0800

    updated with search

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,31 +1,35 @@
 // from data.js
 var tableData = data;
 
-// select table body with d3
 var tbody = d3.select("tbody");
 
-// arrow function to append table rows and table data
-data.forEach((aliens) => {
-    var row = tbody.append("tr");
-    Object.entries(aliens).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+// select table body with d3
+function init(){
+  
 
+  // arrow function to append table rows and table data
+  data.forEach((aliens) => {
+      var row = tbody.append("tr");
+      Object.entries(aliens).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
 // table to html
 var table = d3.select('.table')
 
 table.attr('class', 'table table-bordered table-striped');
 
 // d3 to select filter button and form
-var button = d3.select("button");
-var form = d3.select("input");
+var button = d3.select("#filter-btn");
+var form = d3.select("#datetime");
 
 
 // Event Handlers
 button.on("click", filterData);
 form.on("submit",filterData);
+
 
 
 
@@ -61,17 +65,38 @@ function filterData() {
       });
 
 }
+var search_btn = d3.select('#search-btn');
+var search_form = d3.select("#search");
 
-var search = d3.select('#search')
+search_btn.on('click', filterOnColumns);
+search_form.on('submit', filterOnColumns);
 
-search.on('keyup', filterOnColumns)
 
 function filterOnColumns() {
     
-    var searchInput = search.property('value')
+  var dropdownMenu = d3.select("#selSearch");
+  // Assign the value of the dropdown menu option to a variable
+  var searchColumn = dropdownMenu.property("value");
+  console.log(searchColumn)
+
+
+  var searchInput = search_form.property('value')
     console.log(searchInput)
 
+
+    if (searchColumn === 'city') {
     var columnFiltered = tableData.filter(alien => alien.city === searchInput)
+    }
+    else if (searchColumn === 'state') {
+      var columnFiltered = tableData.filter(alien => alien.state === searchInput)
+      }
+    else if (searchColumn === 'country') {
+      var columnFiltered = tableData.filter(alien => alien.country === searchInput)
+      }
+      else if (searchColumn === 'shape') {
+        var columnFiltered = tableData.filter(alien => alien.shape === searchInput)
+        }
+    
 
         //clear out existing table
         tbody.html('');
@@ -84,6 +109,9 @@ function filterOnColumns() {
               cell.text(value);
             });
           });
-
-
 }
+
+var reset_btn = d3.select('#reset-btn');
+reset_btn.on('click', init);
+
+init()
